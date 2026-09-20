03dbc471b8854f9ffa3bfd934c51581ee0446883
Bezalel <bezuwm@gmail.com>
2026-09-20 17:17:22 -0400

    Cache-bust founding chapter learning-card updates

--- pages/practice.js
+++ pages/practice.js
@@ -8,7 +8,7 @@
    listener and <details>, so it works with the keyboard and a screen reader.
    --------------------------------------------------------------------------- */
 
-import { el, html, esc } from '../layout/dom.js?v=4';
+import { el, html, esc } from '../layout/dom.js?v=5';
 
 function questions() {
   const data = window.EthanStudyHubContent;
