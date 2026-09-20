6baccba27fccd89ea671cf4d6dde951d5e0df2e8
Bezalel <bezuwm@gmail.com>
2026-09-20 17:24:40 -0400

    Cache-bust home story card update

--- pages/course-map.js
+++ pages/course-map.js
@@ -12,8 +12,8 @@
    from the top of the panel alone.
    --------------------------------------------------------------------------- */
 
-import { AREAS, getArea } from '../content/course.js?v=5';
-import { el, html, esc } from '../layout/dom.js?v=5';
+import { AREAS, getArea } from '../content/course.js?v=6';
+import { el, html, esc } from '../layout/dom.js?v=6';
 
 function header() {
   return `
