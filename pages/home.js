5b201825c23110b2652a6eb59484f1ca7ed72660
Bezalel <bezuwm@gmail.com>
2026-09-20 17:17:19 -0400

    Cache-bust founding chapter learning-card updates

--- pages/home.js
+++ pages/home.js
@@ -12,9 +12,9 @@
      render (params) -> HTMLElement
    --------------------------------------------------------------------------- */
 
-import { IMAGES } from '../content/assets.js?v=4';
-import { renderTimeline } from '../components/timeline.js?v=4';
-import { el, html } from '../layout/dom.js?v=4';
+import { IMAGES } from '../content/assets.js?v=5';
+import { renderTimeline } from '../components/timeline.js?v=5';
+import { el, html } from '../layout/dom.js?v=5';
 
 function hero() {
   return html(`
