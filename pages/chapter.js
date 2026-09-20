afcac90cd0ce4c922b3149c12c78d06f1f9d3978
Bezalel <bezuwm@gmail.com>
2026-09-20 17:17:13 -0400

    Cache-bust founding chapter learning-card updates

--- pages/chapter.js
+++ pages/chapter.js
@@ -18,9 +18,9 @@
 
 import {
   CHAPTERS, getChapter, nextChapter, prevChapter, chapterPosition,
-} from '../content/chapters.js?v=4';
-import { renderTimeline } from '../components/timeline.js?v=4';
-import { el, html, esc } from '../layout/dom.js?v=4';
+} from '../content/chapters.js?v=5';
+import { renderTimeline } from '../components/timeline.js?v=5';
+import { el, html, esc } from '../layout/dom.js?v=5';
 
 /* --- Small shared pieces -------------------------------------------------- */
 
